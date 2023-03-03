d72753bf457756043b3a7596287ea0a067093db2
83demon <69196155+83demon@users.noreply.github.com>
2023-03-03 15:46:27 +0200

    Delete test_scraper.py

--- test_scraper.py
+++
@@ -1,6 +0,0 @@
-import os
-#path='/home/vo1/Documents/Programming/PycharmProjects_iPython/Other/Toyota_Corolla/data_analysis/2018_data'
-"""for _,_,files in os.walk(path):
-    for file in files:
-        os.rename(path + '/' + file, path + '/' + f"{file[:file.find('.')]}" + "_2018" + ".jpg")
-"""
